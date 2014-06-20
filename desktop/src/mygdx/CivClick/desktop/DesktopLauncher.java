49eb3ce20ea63366aaa448e689a01d81416cac9f
Tyler <tbakke16@hotmail.com>
2014-06-20 15:45:47 -0700

    First Commit with Libgdx project set up.

---
+++ desktop/src/mygdx/CivClick/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mygdx.CivClick.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mygdx.CivClick.CivClicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CivClicker(), config);
+	}
+}
---
+++ html/src/mygdx/CivClick/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package mygdx.CivClick.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import mygdx.CivClick.CivClicker;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CivClicker();
+        }
+}
